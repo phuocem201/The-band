3da0e8c3a020a94712c9e9d8cc444828d336ee86
phuocem201 <phuocem201@gmail.com>
2022-02-22 16:09:45 +0700

    clone the band

---
+++ assets/JavaScript/app.js
@@ -0,0 +1,21 @@
+// let buyBtns = document.querySelectorAll('.js-buy-btn')
+
+// let modal = document.querySelector('.js-modal')
+// const modalContainer = document.querySelector('.js-modal-container')
+// let modalClose = document.querySelector('.js-modal-close')
+
+// // function showBuyTickets() {
+// //     modal.classList.add('open')
+// // }
+
+// // function hideBuyTickets() {
+// //     modal.classList.remove('open')
+// // }
+
+// // for (const i of buyBtns) {
+// //     modal.addEventListener('click', showBuyTickets)
+// // }
+
+// // modalClose.addEventListener('click', hideBuyTickets)
+
+// // modal.addEventListener('click', hideBuyTickets)
